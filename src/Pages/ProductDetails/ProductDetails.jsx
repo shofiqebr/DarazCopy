ce1faae6b09b6e6b4af728de4797bff468b15fbe
jahangir <jahangiralomtuf99@gmail.com>
2024-05-29 17:39:21 +0600

    [FEAT] - update websitw

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -38,7 +38,10 @@ const ProductDetails = () => {
       image:
         "https://static-01.daraz.com.bd/p/190e7f0df8e9e2edf300eff263b63d49.jpg_750x750.jpg_.webp",
     },
-  ];
+    ];
+    
+
+    
 
   return (
     <div className="max-w-screen-xl mx-auto px-4 mt-5  p-2">
