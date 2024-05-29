950fe240e2dd756078c7c46ee5b22cc2ee9a1a34
jahangir <jahangiralomtuf99@gmail.com>
2024-05-29 17:35:51 +0600

    [FEAT] -update all website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
